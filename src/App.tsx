fd2bd2bd749e05101599d6a7fb2eb0e370fdc6eb
Julia Kozel <julia.kozel.centaurea@a5labs.co>
2023-06-01 13:36:02 +0300

    feat(feat-agent): add form with request

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState<string>('');
+  const [body, setBody] = useState<string>('');
+  const [method, setMethod] = useState<string>('GET');
+  const [requestResponse, setRequestResponse] = useState<string>('');
+
+  enum RequestMethod {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+  }
+  function sendApiRequest() {
+    const requestOptions = {
+      method,
+      headers: { 'Content-Type': 'application/json' },
+      body: method === RequestMethod.GET ? null : body
+  };
+    fetch(url, requestOptions)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setRequestResponse(JSON.stringify(result));
+      },
+      (error) => {
+        setRequestResponse(JSON.stringify(error));
+      }
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+        <div className="App-form">
+          <div className="App-field">
+            <label className="App-label" htmlFor="url">Url</label>
+            <input id="url" className="App-input" value={url} onChange={e => setUrl(e.target.value)} />
+          </div>
+          <div className="App-field">
+            <label className="App-label" htmlFor="method">Method</label>
+            <select id="method"  className="App-input" value={method} onChange={e => setMethod(e.target.value)}>
+              {Object.values(RequestMethod).map(value => {
+                return <option value={value}>{value}</option>;
+              })}
+            </select>
+          </div>
+          <div className="App-field">
+            <label className="App-label" htmlFor="body">Body json</label>
+            <input id="body" className="App-input" value={body} onChange={e => setBody(e.target.value)} />
+          </div>
+          <button className="App-button" onClick={sendApiRequest}>Send Request</button>
+          <div className="App-field">
+            <label className="App-label" htmlFor="result">Response</label>
+            <textarea id="result" className="App-textarea" rows={10} cols={80} defaultValue={requestResponse} />
+          </div>
+        </div>
       </header>
     </div>
   );
